4ac5665264644b57fbe2fc4dea24f6a7686842cd
Koinunopochi <koinunopoti@gmail.com>
2024-11-17 23:32:08 +0900

    feature : 最低限ビルドできるだろう状態を作成した

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      // 型定義ファイルの生成
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: '@tetex/ui',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format}.js`,
+    },
+     rollupOptions: {
+      external: ['react', 'react-dom', 'tailwindcss'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        // 固定の文字列を指定
+        assetFileNames: 'index.css',
+      },
+    },
+    },
+  },
+});
